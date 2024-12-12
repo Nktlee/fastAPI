686bdc807851f5d058775005388d03ef6f2d6e8c
Nikita Lee <mrmitik.2003@gmail.com>
2024-12-12 14:56:04 +0300

    fix bugs with auth

--- src/migrations/versions/2024_12_12_1449-ea49ff53afd7_add_users.py
+++ src/migrations/versions/2024_12_12_1449-ea49ff53afd7_add_users.py
@@ -1,8 +1,8 @@
 """add users
 
-Revision ID: 768caa0f39b6
+Revision ID: ea49ff53afd7
 Revises: 4044a97d9182
-Create Date: 2024-12-12 13:23:11.533994
+Create Date: 2024-12-12 14:49:17.382804
 
 """
 
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = "768caa0f39b6"
+revision: str = "ea49ff53afd7"
 down_revision: Union[str, None] = "4044a97d9182"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -24,7 +24,7 @@ def upgrade() -> None:
         "users",
         sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("email", sa.String(length=100), nullable=False),
-        sa.Column("password", sa.String(length=100), nullable=False),
+        sa.Column("hashed_password", sa.String(length=100), nullable=False),
         sa.PrimaryKeyConstraint("id"),
     )
 
