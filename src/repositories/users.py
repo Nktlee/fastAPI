52a80117b7416fb2d16f6f2ffd903cfd977d3436
Nikita Lee <mrmitik.2003@gmail.com>
2024-12-13 19:24:11 +0300

    add password verification

--- src/repositories/users.py
+++ src/repositories/users.py
@@ -1,4 +1,7 @@
-from src.schemas.users import User
+from pydantic import EmailStr
+from sqlalchemy import select
+
+from src.schemas.users import User, UsersWithHashedPassword
 from src.repositories.base import BaseRepository
 from src.models.users import UsersOrm
 
@@ -6,3 +9,10 @@ from src.models.users import UsersOrm
 class UsersRepository(BaseRepository):
     model = UsersOrm
     schema = User
+
+    async def get_user_with_hashed_password(self, email: EmailStr):
+        query = select(self.model).filter_by(email=email)
+        result = await self.session.execute(query)
+        model = result.scalars().one()
+
+        return UsersWithHashedPassword.model_validate(model, from_attributes=True)
--- src/schemas/users.py
+++ src/schemas/users.py
@@ -16,3 +16,6 @@ class User(BaseModel):
     email: EmailStr
 
     model_config = ConfigDict(from_attributes=True)
+
+class UsersWithHashedPassword(User):
+    hashed_password: str
