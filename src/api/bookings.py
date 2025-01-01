2304fd3479f7d20d8cf468aa7e7493ae9741a516
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-01 23:46:37 +0300

    add router get_bookings

--- src/api/bookings.py
+++ src/api/bookings.py
@@ -7,19 +7,23 @@ from src.schemas.bookings import BookingRequestAdd, BookingAdd
 router = APIRouter(prefix="/bookings", tags=["Бронирования"])
 
 
-@router.post("")
-async def add_booking(
-    user_id: UserIdDep,
-    db: DBDep,
-    booking_data: BookingRequestAdd
-):
+@router.get("", summary="Получение всех бронирований")
+async def get_bookings(db: DBDep):
+    return await db.bookings.get_all()
+
+
+@router.get("/me", summary="Получение всех бронирований пользователя")
+async def get_my_bookings(db: DBDep, user_id: UserIdDep):
+    return await db.bookings.get_filtered(user_id=user_id)
+
+
+@router.post("", summary="Добавление бронирований")
+async def add_booking(user_id: UserIdDep, db: DBDep, booking_data: BookingRequestAdd):
     room = await db.rooms.get_one_or_none(id=booking_data.room_id)
     room_price = room.price
 
     _booking_data = BookingAdd(
-        user_id=user_id,
-        price=room_price,
-        **booking_data.model_dump()
+        user_id=user_id, price=room_price, **booking_data.model_dump()
     )
     booking = await db.bookings.add(_booking_data)
     await db.commit()
