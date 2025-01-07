de11a13adbdfb42778b0b2173f6a05c65018a7aa
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-07 21:51:41 +0300

    add filter by time to get hotels

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,5 +1,9 @@
+from datetime import date
+
 from sqlalchemy import select, func
 
+from src.models.rooms import RoomsOrm
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
 from src.repositories.base import BaseRepository
 from src.models.hotels import HotelsOrm
@@ -26,3 +30,13 @@ class HotelsRepository(BaseRepository):
             Hotel.model_validate(hotel, from_attributes=True)
             for hotel in result.scalars().all()
         ]
+
+    async def get_filtered_by_time(self, date_from: date, date_to: date):
+        rooms_ids_to_get = rooms_ids_for_booking(date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,12 +1,9 @@
 from datetime import date
 
-from sqlalchemy import func, select
-
-from src.models.bookings import BookingsOrm
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.rooms import Room
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
-from src.database import engine
 
 
 class RoomsRepository(BaseRepository):
@@ -14,41 +11,6 @@ class RoomsRepository(BaseRepository):
     schema = Room
 
     async def get_filtered_by_time(self, hotel_id: int, date_from: date, date_to: date):
-        rooms_count = (
-            select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
-            .select_from(BookingsOrm)
-            .filter(
-                BookingsOrm.date_from <= date_from,
-                BookingsOrm.date_to >= date_to,
-            )
-            .group_by(BookingsOrm.room_id)
-            .cte(name="room_count")
-        )
-
-        rooms_left_table = (
-            select(
-                RoomsOrm.id.label("room_id"),
-                (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left"),
-            )
-            .select_from(RoomsOrm)
-            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
-            .cte(name="rooms_left_table")
-        )
-
-        rooms_ids_for_hotel = (
-            select(RoomsOrm.id)
-            .select_from(RoomsOrm)
-            .filter_by(hotel_id=hotel_id)
-            .subquery(name="rooms_ids_for_hotel")
-        )
-
-        rooms_ids_to_get = (
-            select(rooms_left_table.c.room_id)
-            .select_from(rooms_left_table)
-            .filter(
-                rooms_left_table.c.rooms_left > 0,
-                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel)
-            )
-        )
+        rooms_ids_to_get = rooms_ids_for_booking(hotel_id, date_from, date_to)
 
         return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
