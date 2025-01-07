e662efbec11e028b7e804e5b0862a2ce84c0fe12
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-07 21:53:50 +0300

    fix bug

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -11,6 +11,6 @@ class RoomsRepository(BaseRepository):
     schema = Room
 
     async def get_filtered_by_time(self, hotel_id: int, date_from: date, date_to: date):
-        rooms_ids_to_get = rooms_ids_for_booking(hotel_id, date_from, date_to)
+        rooms_ids_to_get = rooms_ids_for_booking(date_from, date_to, hotel_id)
 
         return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
