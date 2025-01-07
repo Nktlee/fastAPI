883ef19a1b704312ea1a88c2f472b1d6dd761ed2
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-07 21:07:18 +0300

    add complex query with filters to get rooms

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -1,22 +1,42 @@
+from datetime import date
+
+from sqlalchemy import func, select
+
+from src.models.bookings import BookingsOrm
 from src.schemas.rooms import Room
 from src.repositories.base import BaseRepository
 from src.models.rooms import RoomsOrm
+from src.database import engine
 
 
 class RoomsRepository(BaseRepository):
     model = RoomsOrm
     schema = Room
 
-    # async def get_all(
-    #         self,
-    #         hotel_id: int,
-    #         title: str,
-    # ) -> list[Room]:
-    #     query = select(RoomsOrm)
-    #     if hotel_id:
-    #         query = query.filter(RoomsOrm.hotel_id == hotel_id)
-    #     if title:
-    #         query = query.filter(func.lower(RoomsOrm.title).contains(title.strip().lower()))
-    #     result = await self.session.execute(query)
+    async def get_filtered_by_time(self, hotel_id: int, date_from: date, date_to: date):
+        rooms_count = (
+            select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
+            .select_from(BookingsOrm)
+            .filter(
+                BookingsOrm.date_from <= date_from,
+                BookingsOrm.date_to >= date_to,
+            )
+            .group_by(BookingsOrm.room_id)
+            .cte(name="room_count")
+        )
 
-    #     return [Room.model_validate(room, from_attributes=True) for room in result.scalars().all()]
+        rooms_left_table = (
+            select(
+                RoomsOrm.id.label("room_id"),
+                (RoomsOrm.quantity - func.coalesce(rooms_count.c.rooms_booked, 0)).label("rooms_left"),
+            )
+            .select_from(RoomsOrm)
+            .outerjoin(rooms_count, RoomsOrm.id == rooms_count.c.room_id)
+            .cte(name="rooms_left_table")
+        )
+
+        query = (
+            select(rooms_left_table)
+            .select_from(rooms_left_table)
+            .filter(rooms_left_table.c.rooms_left > 0)
+        )
