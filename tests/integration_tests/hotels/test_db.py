306ee101872c98d44c063a9658175a0aa57fb272
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-20 12:31:44 +0300

    add conftest

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -6,6 +6,5 @@ from src.database import async_session_maker
 async def test_add_hotel():
     hotel_data = HotelAdd(title="Hotel 1", location="Сочи")
     async with DBManager(session_factory=async_session_maker) as db:
-        new_hotel_data = await db.hotels.add(hotel_data)
-
-    print(f"{new_hotel_data=}")
+        await db.hotels.add(hotel_data)
+        await db.commit()
