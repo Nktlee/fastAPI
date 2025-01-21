5621e4221ffe187c72d21e3e13fa296d35019ac6
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-21 11:22:38 +0300

    add facilities api tests

---
+++ tests/integration_tests/facilities/test_api.py
@@ -0,0 +1,7 @@
+async def test_get_facilities(ac):
+    response = await ac.get("/facilities")
+    assert response.status_code == 200
+
+async def test_post_facilities(ac):
+    response = await ac.post("/facilities", json={"title": "facility"})
+    assert response.status_code == 200 
