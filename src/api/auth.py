996639e652baa56bf0b9036fc0e90a063c4b0101
Nikita Lee <mrmitik.2003@gmail.com>
2025-01-23 09:22:24 +0300

    add api test auth

--- src/api/auth.py
+++ src/api/auth.py
@@ -2,19 +2,24 @@ from fastapi import APIRouter, HTTPException, Response
 
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
-from src.database import async_session_maker
+from src.database import async_session_maker_null_pool
 from src.services.auth import AuthService
-from src.api.dependencies import UserIdDep
+from src.api.dependencies import UserIdDep, DBDep
 
 
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 
 @router.post("/register")
-async def register_user(data: UserRequestAdd):
+async def register_user(db: DBDep, data: UserRequestAdd):
+    users = await db.users.get_all()
+    emails = [user.email for user in users]
+    if data.email in emails:
+        raise HTTPException(status_code=400, detail="Пользователь уже существует")
+
     hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
-    async with async_session_maker() as session:
+    async with async_session_maker_null_pool() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
 
@@ -23,14 +28,13 @@ async def register_user(data: UserRequestAdd):
 
 @router.post("/login")
 async def login_user(data: UserRequestAdd, response: Response):
-    async with async_session_maker() as session:
-        user = await UsersRepository(session).get_user_with_hashed_password(
-            email=data.email
-        )
-        if not user:
-            raise HTTPException(
-                status_code=401, detail="Пользователь с таким email не зарегистрирован"
+    async with async_session_maker_null_pool() as session:
+        try:
+            user = await UsersRepository(session).get_user_with_hashed_password(
+                email=data.email
             )
+        except:
+            raise HTTPException(status_code=401, detail="Пользователь с таким email не зарегистрирован")
         if not AuthService().verify_password(data.password, user.hashed_password):
             raise HTTPException(status_code=401, detail="Пароль неверный")
 
@@ -42,7 +46,7 @@ async def login_user(data: UserRequestAdd, response: Response):
 
 @router.get("/me")
 async def get_me(user_id: UserIdDep):
-    async with async_session_maker() as session:
+    async with async_session_maker_null_pool() as session:
         user = await UsersRepository(session).get_one_or_none(id=user_id)
         return user
 
---
+++ tests/integration_tests/auth/test_api.py
@@ -0,0 +1,61 @@
+from httpx import AsyncClient
+
+import pytest
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("test@test.com", "1234", 200),
+    ("test@example.com", "password123", 200),
+    ("test2@example.com", "password123", 200),
+    ("test@test.ru", "1234", 400),
+])
+async def test_register_user(
+    email: str,
+    password: str,
+    status_code: int,
+    authenticated_ac: AsyncClient,
+):
+    response_register = await authenticated_ac.post(
+        "/auth/register",
+        json={
+            "email": email,
+            "password": password
+        }
+    )
+    assert response_register.status_code == status_code
+
+
+@pytest.mark.parametrize("email, password, status_code", [
+    ("test@test.com", "1234", 200),
+    ("test@example.com", "password123", 200),
+    ("test2@example.com", "password123", 200),
+    ("nktlee@example.com", "password123", 401),
+    ("test2@example.com", "password12", 401),
+])
+async def test_login_user(
+    email: str,
+    password: str,
+    status_code: int,
+    authenticated_ac: AsyncClient,
+):
+    response_login = await authenticated_ac.post(
+        "/auth/login",
+        json={
+            "email": email,
+            "password": password
+        }
+    )
+    assert response_login.status_code == status_code
+    if status_code == 200:
+        assert "access_token" in response_login.json()
+
+        response_get_me = await authenticated_ac.get("/auth/me")
+        assert response_get_me.json()["email"] == email
+
+
+async def test_logout_user(authenticated_ac: AsyncClient):
+    response_logout = await authenticated_ac.post("/auth/logout")
+    assert response_logout.status_code == 200
+
+    response_get_me = await authenticated_ac.get("/auth/me")
+    assert response_get_me.status_code == 401
